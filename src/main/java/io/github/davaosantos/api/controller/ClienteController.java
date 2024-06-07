92818df02515891cd5789dca782522de5b0b765a
david <david_Alisson60@yahoo.com.br>
2024-06-06 22:26:53 -0300

    Aula S.4 40 -> Deletando clientes

--- src/main/java/io/github/davaosantos/api/controller/ClienteController.java
+++ src/main/java/io/github/davaosantos/api/controller/ClienteController.java
@@ -2,25 +2,52 @@ package io.github.davaosantos.api.controller;
 
 
 import io.github.davaosantos.domain.entity.Cliente;
+import io.github.davaosantos.domain.repository.ClientesRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-            value = "/hello/{nome}",
-            method = RequestMethod.GET,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"}
-    )
+    private ClientesRepository clientesRepository;
+
+    public ClienteController(ClientesRepository clientesRepository) {
+        this.clientesRepository = clientesRepository;
+    }
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nmCliente){
-        return String.format("Hello %s ", nmCliente);
+    public ResponseEntity getClienteById(@PathVariable("id") Integer idCliente){
+        Optional<Cliente> cliente = clientesRepository.findById(idCliente);
+
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
     };
+
+    @PostMapping("/salvar")
+    @ResponseBody
+    public ResponseEntity saveCliente(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientesRepository.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    };
+
+    @DeleteMapping("/deletar/{id}")
+    @ResponseBody
+    public ResponseEntity deletar(@PathVariable("id") Integer idCliente){
+        Optional<Cliente> cliente = clientesRepository.findById(idCliente);
+
+        if (cliente.isPresent()){
+            clientesRepository.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
--- src/main/java/io/github/davaosantos/domain/entity/Cliente.java
+++ src/main/java/io/github/davaosantos/domain/entity/Cliente.java
@@ -1,6 +1,8 @@
 package io.github.davaosantos.domain.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -16,6 +18,7 @@ public class Cliente {
     @Column(name = "NOME", length = 100)
     private String nome;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 
