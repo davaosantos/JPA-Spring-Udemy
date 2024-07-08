cb766384da47b6dbbdc9c52594daf446874f96f4
David Alisson <david@infotera.com.br>
2024-07-07 21:54:22 -0300

    53. Spring Dev Tools

--- src/main/java/io/github/davaosantos/api/controller/PedidoController.java
+++ src/main/java/io/github/davaosantos/api/controller/PedidoController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
@@ -23,6 +25,11 @@ public class PedidoController {
     public Integer salvarPedido(@RequestBody PedidoDTO pedidoDTO){
         Pedido pedido = pedidoService.salvar(pedidoDTO);
         return pedido.getId();
+    }
+
+    @GetMapping("/ola")
+    public String testarDevTools(){
+        return "Ola devtools";
     }
 
 }
