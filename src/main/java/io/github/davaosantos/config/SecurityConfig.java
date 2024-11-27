846f3a75bed6c1cbea590cf64e5830b7cdeb8707
david <david_Alisson60@yahoo.com.br>
2024-11-27 13:49:29 -0300

    S.8 - 281 - Password Encoder

--- src/main/java/io/github/davaosantos/config/SecurityConfig.java
+++ src/main/java/io/github/davaosantos/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package io.github.davaosantos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //$2a$12$DgJnUyI3z.8lDaSxpMEycOJjgSCuq6OWcxR/fU7tQ4/m5Kd2KZ7Ku
+   // $2a$12$L9rkliB.japWWo1Rva3PtOrneDrA4me/dyU/9jxYaPVfYEnyWZsii
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    };
 
     //Autenticação
     @Override
